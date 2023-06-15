721b2a62796321333f63b33814564b9286b1d894
Gabriel Teodoro <teodorogabriel367@gmail.com>
2023-06-15 19:35:18 -0300

    1.10

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const tela = document.querySelector('.pj');
 function troca(){
     if(count < projetos.length-1){
         count++
-        tela.innerHTML = `<video controls autoplay width="550px" src="videos/${projetos[count]}.mp4"></video>`
+        tela.innerHTML = `<video muted controls autoplay width="550px" src="videos/${projetos[count]}.mp4"></video>`
     }
     else{
         count = 0
-        tela.innerHTML = `<video controls autoplay width="550px" src="videos/${projetos[count]}.mp4"></video>`
+        tela.innerHTML = `<video muted controls autoplay width="550px" src="videos/${projetos[count]}.mp4"></video>`
     }
 }
 var count = 0;
 
-var projetos = [0, 1, 2, 3, 4]
+var projetos = [0, 1, 2, 3, 4, 5, 6, 7]
